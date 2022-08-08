37dbc21ce2a1e9bce976756fb678998f80742971
Maik Wosnitzka <pixorama@live.de>
2022-08-08 19:31:27 +0200

    Expanded MetaModelFactoryTest to test invalid graph element definitions

--- java-graph-persistence-utils/src/test/java/de/x1285/jgp/tests/MetaModelFactoryTest.java
+++ java-graph-persistence-utils/src/test/java/de/x1285/jgp/tests/MetaModelFactoryTest.java
@@ -2,6 +2,8 @@ package de.x1285.jgp.tests;
 
 import de.x1285.jgp.api.annotation.Edge;
 import de.x1285.jgp.api.annotation.Property;
+import de.x1285.jgp.element.GraphEdge;
+import de.x1285.jgp.element.GraphElement;
 import de.x1285.jgp.element.GraphVertex;
 import de.x1285.jgp.metamodel.MetaModel;
 import de.x1285.jgp.metamodel.MetaModelException;
@@ -118,15 +120,74 @@ public class MetaModelFactoryTest {
     }
 
     @Test
-    public void testInvalidVertexClass() {
-        InvalidVertex invalidVertex = new InvalidVertex();
+    public void testInvalidVertexWithVertexPropertyClass() {
+        InvalidVertexWithVertexProperty invalidVertex = new InvalidVertexWithVertexProperty();
+        assertThrows(MetaModelException.class, () -> MetaModelFactory.createMetaModel(invalidVertex));
+    }
+
+    @Test
+    public void testInvalidVertexWithEdgePropertyClass() {
+        InvalidVertexWithEdgeProperty invalidVertex = new InvalidVertexWithEdgeProperty();
+        assertThrows(MetaModelException.class, () -> MetaModelFactory.createMetaModel(invalidVertex));
+    }
+
+    @Test
+    public void testInvalidEdgeWithEdgeFieldClass() {
+        InvalidEdgeWithEdgeField invalidEdge = new InvalidEdgeWithEdgeField();
+        assertThrows(MetaModelException.class, () -> MetaModelFactory.createMetaModel(invalidEdge));
+    }
+
+    @Test
+    public void testInvalidEdgeWithVertexFieldClass() {
+        InvalidEdgeWithVertexField invalidEdge = new InvalidEdgeWithVertexField();
+        assertThrows(MetaModelException.class, () -> MetaModelFactory.createMetaModel(invalidEdge));
+    }
+
+    @Test
+    public void testInvalidVertexWithNonGraphElementEdgeClass() {
+        InvalidVertexWithNonGraphElementEdge invalidVertex = new InvalidVertexWithNonGraphElementEdge();
         assertThrows(MetaModelException.class, () -> MetaModelFactory.createMetaModel(invalidVertex));
     }
 
     @Getter
     @Setter
+    private static class InvalidVertexWithVertexProperty extends GraphVertex {
+        @Property // INVALID: A property can not be a vertex
+        private GraphVertex notAValidProperty;
+    }
+
+    @Getter
+    @Setter
+    private static class InvalidVertexWithEdgeProperty extends GraphVertex {
+        @Property // INVALID: A property can not be an edge
+        private GraphEdge<?, ?> notAValidProperty;
+    }
+
+    @Getter
+    @Setter
     private static class InvalidVertex extends GraphVertex {
-        @Property
+        @Property // INVALID: A property can not be a vertex
         private InvalidVertex notAValidProperty;
+    }
+
+    @Getter
+    @Setter
+    private static class InvalidEdgeWithEdgeField extends GraphEdge<InvalidVertex, InvalidVertex> {
+        @Edge // INVALID: An edge can not have an edge
+        private GraphEdge<?, ?> notAValidEdge;
+    }
+
+    @Getter
+    @Setter
+    private static class InvalidEdgeWithVertexField extends GraphEdge<InvalidVertex, InvalidVertex> {
+        @Edge // INVALID: An edge can not have an edge
+        private GraphVertex notAValidEdge;
+    }
+
+    @Getter
+    @Setter
+    private static class InvalidVertexWithNonGraphElementEdge extends GraphVertex {
+        @Edge // INVALID: An edge needs to be instanceof GraphEdge, GraphVertex or a collection of these
+        private String notAValidEdge;
     }
 }
